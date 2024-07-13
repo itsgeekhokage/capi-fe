b8a45e26f9d7c1979f2d0cafc93a2ecc7fbeddae
Shivam Prajapati <100841132+itsgeekhokage@users.noreply.github.com>
2024-07-13 23:23:58 +0530

    new repo

--- src/adminpanel/question_tags/EditTags.jsx
+++ src/adminpanel/question_tags/EditTags.jsx
@@ -10,11 +10,6 @@ import {
   Checkbox,
   FormControl,
   FormControlLabel,
-  List,
-  ListItem,
-  ListItemText,
-  ListItemSecondaryAction,
-  Collapse,
   InputLabel,
   Select,
   MenuItem,
@@ -22,6 +17,8 @@ import {
 import { AddCircleTwoTone, Queue, Remove, Add } from "@mui/icons-material";
 import React, { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import Dynamic_Type from "./Dynamic_Type";
+import Static2_Type from "./Static2_Type";
 
 const EditTag = () => {
   const theme = useTheme();
@@ -41,8 +38,14 @@ const EditTag = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      let { id, isDefault, tag, placeholder, desposition_type, desposition } =
-        location.state;
+      let {
+        id,
+        default: isDefault,
+        tag,
+        placeholder,
+        desposition_type,
+        desposition,
+      } = location.state;
       desposition = await JSON.parse(desposition);
       setFormData({
         isDefault,
@@ -183,169 +186,7 @@ const EditTag = () => {
   };
 
   const [isShortAnswer, setIsShortAnswer] = useState(true);
-  const [options, setOptions] = useState([
-    {
-      id: 1,
-      text: "",
-      comments: false,
-      sublist: [],
-    },
-  ]);
-  const [selectedOption, setSelectedOption] = useState(null);
-  const [openSublists, setOpenSublists] = useState({});
-  const [hoveredOption, setHoveredOption] = useState(null);
-  const [width, setWidth] = useState(500);
-  const sidebarRef = useRef(null);
-  const isResizing = useRef(false);
-
-  const handleMouseDown = () => {
-    isResizing.current = true;
-  };
-
-  const handleMouseMove = (e) => {
-    if (isResizing.current) {
-      const newWidth =
-        e.clientX - sidebarRef.current.getBoundingClientRect().left;
-      if (newWidth > 100 && newWidth < 800) {
-        setWidth(newWidth);
-      }
-    }
-  };
-
-  const handleMouseUp = () => {
-    isResizing.current = false;
-  };
-
-  const toggleSublist = (id) => {
-    setOpenSublists((prev) => ({
-      ...prev,
-      [id]: !prev[id],
-    }));
-  };
-
-
-   const handleAddOption = (parentOption = null, isSubOption = false) => {
-     const newOption = {
-       id: Date.now(),
-       text: "",
-       comments: false,
-       sublist: [],
-     };
-     if (parentOption && parentOption.comments) return;
-     if (isSubOption && parentOption) {
-       parentOption.sublist.push(newOption);
-       setOpenSublists((prev) => ({
-         ...prev,
-         [parentOption.id]: true,
-       }));
-     } else if (parentOption) {
-       const parentIndex = options.findIndex(
-         (opt) => opt.id === parentOption.id
-       );
-       options.splice(parentIndex + 1, 0, newOption);
-     } else {
-       setOptions([...options, newOption]);
-     }
-     setSelectedOption(newOption);
-     setOptions([...options]);
-   };
-
-
-  const handleRemoveOption = (optionToRemove, parentOption) => {
-    if (parentOption) {
-      parentOption.sublist = parentOption.sublist.filter(
-        (option) => option.id !== optionToRemove.id
-      );
-      setOptions([...options]);
-    } else {
-      setOptions(options.filter((option) => option.id !== optionToRemove.id));
-    }
-    if (selectedOption && selectedOption.id === optionToRemove.id) {
-      setSelectedOption(null);
-    }
-  };
-
-  const handleOptionChange = (optionToUpdate, key, value) => {
-    optionToUpdate[key] = value;
-    setOptions([...options]);
-  };
-
-  const handleHasComment = (optionToUpdate, value) => {
-    optionToUpdate["comments"] = value;
-    optionToUpdate.sublist = [];
-    setOptions([...options]);
-  };
-
-  const handleOptionSelect = (option) => {
-    setSelectedOption(option);
-  };
-
-  const renderOption = (option, parentOption = null) => (
-    <Box
-      key={option.id}
-      sx={{ marginLeft: parentOption ? "1rem" : "0", padding: "0.25rem" }}
-      onMouseEnter={() => setHoveredOption(option.id)}
-      onMouseLeave={() => setHoveredOption(null)}>
-      <ListItem
-        component={Box}
-        elevation={selectedOption === option ? 3 : 1}
-        sx={{
-          backgroundColor:
-            selectedOption === option
-              ? "info.main"
-              : theme.palette.background.default,
-          borderRadius: "4px",
-          cursor: "pointer",
-          padding: "0 5px",
-        }}
-        onClick={() => {
-          handleOptionSelect(option);
-          toggleSublist(option.id);
-        }}>
-        <ListItemText>
-          <TextField
-            value={option.text}
-            onChange={(e) => handleOptionChange(option, "text", e.target.value)}
-            size="small"
-            placeholder="Option text"
-            fullWidth
-            variant="standard"
-          />
-        </ListItemText>
-        <ListItemSecondaryAction
-          sx={{ display: hoveredOption === option.id ? "block" : "none" }}>
-          {/* <Add
-            fontSize="small"
-            sx={{ cursor: "pointer" }}
-            onClick={() => handleAddOption(option, false)}
-          /> */}
-          <Queue
-            fontSize="small"
-            sx={{ cursor: "pointer" }}
-            onClick={(e) => {
-              e.stopPropagation();
-              handleAddOption(option, true);
-            }}
-          />
-          <Remove
-            fontSize="small"
-            sx={{ cursor: "pointer" }}
-            onClick={(e) => {
-              e.stopPropagation();
-              handleRemoveOption(option, parentOption);
-            }}
-          />
-        </ListItemSecondaryAction>
-      </ListItem>
-      <Collapse in={openSublists[option.id]}>
-        {option.sublist.length > 0 && (
-          <List sx={{ padding: 0 }}>
-            {option.sublist.map((subOption) => renderOption(subOption, option))}
-          </List>
-        )}
-      </Collapse>
-    </Box>
-  );
+  const [options, setOptions] = useState([]);
 
   return (
     <Box>
@@ -470,79 +311,19 @@ const EditTag = () => {
           />
         )}
       </Box>
-
-      <Box
-        sx={{ display: "flex", marginTop: "2rem" }}
-        onMouseMove={handleMouseMove}
-        onMouseUp={handleMouseUp}>
-        <Box
-          ref={sidebarRef}
-          sx={{
-            width: `${width}px`,
-            transition: "width 0.1s ease-out",
-            overflow: "hidden",
-            display: "flex",
-            flexDirection: "column",
-            borderRight: "1px solid #ccc",
-            position: "relative",
-            marginRight: "1rem",
-          }}>
-          <Box
-            sx={{
-              width: "5px",
-              cursor: "ew-resize",
-              position: "absolute",
-              right: 0,
-              top: 0,
-              bottom: 0,
-              backgroundColor: "#ddd",
-              "&:hover": {
-                width: "8px",
-              },
-            }}
-            onMouseDown={handleMouseDown}
-          />
-          <Button
-            onClick={handleAddOption}
-            startIcon={<AddCircleTwoTone />}
-            fullWidth
-            color="info"
-            sx={{ marginBottom: "1rem" }}>
-            Add Option
-          </Button>
-          <List>{options.map((option) => renderOption(option))}</List>
-        </Box>
-        <Box sx={{ flexGrow: 1, paddingLeft: "1rem" }}>
-          {selectedOption && (
-            <Box sx={{ marginTop: "2rem" }}>
-              <Typography variant="h6">Edit Option</Typography>
-              <TextField
-                fullWidth
-                value={selectedOption.text}
-                onChange={(e) =>
-                  handleOptionChange(selectedOption, "text", e.target.value)
-                }
-                placeholder="Option text"
-                size="small"
-                sx={{ marginBottom: "1rem" }}
-              />
-              <FormControlLabel
-                control={
-                  <Switch
-                    color="info"
-                    size="small"
-                    checked={selectedOption.comments}
-                    onChange={(e) =>
-                      handleHasComment(selectedOption, e.target.checked)
-                    }
-                  />
-                }
-                label="Add Comments"
-              />
-            </Box>
-          )}
-        </Box>
-      </Box>
+      {formData.desposition_type == "static" && ""}
+      {formData.desposition_type == "dynamic" && (
+        <Dynamic_Type
+          options={options}
+          setOptions={setOptions}
+        />
+      )}
+      {formData.desposition_type == "static2" && (
+        <Static2_Type
+          options={options}
+          setOptions={setOptions}
+        />
+      )}
     </Box>
   );
 };
